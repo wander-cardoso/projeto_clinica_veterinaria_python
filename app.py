b67376c7db7bed8660619fdca47997ed109be12b
Wander Cardoso <159091312+wander-cardoso@users.noreply.github.com>
2026-06-18 15:50:57 +0100

    Remove dotenv import and loading
    Remove dotenv loading from app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import mysql.connector
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 # ---------- BD ----------
 
 def ligar_bd():
